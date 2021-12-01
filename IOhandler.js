8c5917bd23b5e44ff3892a2bc803da3039f1c8a8
ayiu93 <anthonyyiu93@gmail.com>
2021-12-01 06:36:12 -0800

    done

--- IOhandler.js
+++ IOhandler.js
@@ -3,8 +3,8 @@
  * File Name: IOhandler.js
  * Description: Collection of functions for files input/output related operations
  *
- * Created Date:
- * Author:
+ * Created Date: Nov. 30, 2021
+ * Author: Anthony Yiu
  *
  */
 
@@ -20,7 +20,17 @@ const unzipper = require("unzipper"),
  * @param {string} pathOut
  * @return {promise}
  */
-const unzip = (pathIn, pathOut) => {};
+const unzip = (pathIn, pathOut) => {
+  return new Promise ((resolve, reject) => {
+    fs.createReadStream(pathIn)
+      .pipe(unzipper.Extract({ path: pathOut }))
+      .on('close', () => {
+        console.log("Extraction operation complete")
+        resolve(pathOut)
+      })
+      .on('error', err => reject(err))
+  })
+};
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path
@@ -28,7 +38,21 @@ const unzip = (pathIn, pathOut) => {};
  * @param {string} path
  * @return {promise}
  */
-const readDir = (dir) => {};
+const readDir = (dir) => {
+  return new Promise ((resolve, reject) => {
+    fs.readdir(dir, (err, files) => {
+      pngArray = []
+      files.forEach(file => {
+        if(path.extname(file) == '.png')
+          pngArray.push(path.join(dir, path.basename(file)))
+      })
+      if (err)
+        reject(err)
+        
+      resolve(pngArray)
+    })
+  })
+};
 
 /**
  * Description: Read in png file by given pathIn,
@@ -38,7 +62,28 @@ const readDir = (dir) => {};
  * @param {string} pathProcessed
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {};
+const grayScale = (pathIn, pathOut) => {
+  return new Promise ((resolve, reject) => { 
+      fs.createReadStream(pathIn)
+        .pipe(new PNG())
+        .on("parsed", function () {
+          for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
+              let idx = (this.width * y + x) << 2;
+              let avg = (this.data[idx] + this.data[idx+1] + this.data[idx+2]) / 3
+     
+              this.data[idx] = avg;
+              this.data[idx + 1] = avg;
+              this.data[idx + 2] = avg;
+            }
+          }
+          resolve(this.pack().pipe(fs.createWriteStream(path.join(pathOut, path.basename(pathIn)))),
+            console.log(`${path.basename(pathIn)} complete`));
+        })
+        //.on('error', (error) => console.log(error))
+        .on('error', err => reject(err))
+  })
+};
 
 module.exports = {
   unzip,
--- main.js
+++ main.js
@@ -3,8 +3,8 @@
  * File Name: main.js
  * Description:
  *
- * Created Date:
- * Author:
+ * Created Date: Nov. 30, 2021
+ * Author: Anthony Yiu
  *
  */
 
@@ -12,3 +12,10 @@ const IOhandler = require("./IOhandler"),
   zipFilePath = `${__dirname}/myfile.zip`,
   pathUnzipped = `${__dirname}/unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
+
+IOhandler.unzip(zipFilePath, pathUnzipped)
+  .then((dir) => IOhandler.readDir(dir))
+  .then((images) => { images.forEach(image => {
+    IOhandler.grayScale(image, pathProcessed)
+  })})
+  .catch((err) => console.log(err));
